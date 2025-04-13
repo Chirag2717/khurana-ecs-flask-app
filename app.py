4ac6166aad74b984e5598782a368979bd719581c
Chirag2717 <chirag.khurana@dcmail.ca>
2025-04-13 10:14:28 -0400

    Initial commit - Flask app containerized

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello from Khurana ECS Container."
+
+@app.route('/host')
+def hostname():
+    return f"Hostname: {socket.gethostname()}"
+
+@app.route('/ip')
+def ip():
+    return f"Internal IP: {socket.gethostbyname(socket.gethostname())}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
